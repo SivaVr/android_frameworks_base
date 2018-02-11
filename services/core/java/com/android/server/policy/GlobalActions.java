b1fae0853e8d2752bf067c4ea14c89d0d2a615a5
SivaVr <36237000+ssj4vr@users.noreply.github.com>
2018-02-11 12:42:31 +0530

    init color engine

--- services/core/java/com/android/server/policy/GlobalActions.java
+++ services/core/java/com/android/server/policy/GlobalActions.java
@@ -31,6 +31,8 @@ import com.android.internal.widget.LockPatternUtils;
 import android.app.ActivityManager;
 import android.app.ActivityManagerNative;
 import android.app.Dialog;
+import android.app.IThemeCallback;
+import android.app.ThemeManager;
 import android.app.INotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
@@ -111,7 +113,7 @@ import static com.android.internal.util.cm.PowerMenuConstants.*;
  * may show depending on whether the keyguard is showing, and whether the device
  * is provisioned.
  */
-class GlobalActions implements DialogInterface.OnDismissListener, DialogInterface.OnClickListener  {
+public class GlobalActions implements DialogInterface.OnDismissListener, DialogInterface.OnClickListener  {
 
     private static final String TAG = "GlobalActions";
 
@@ -147,7 +149,19 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
 
     private BitSet mAirplaneModeBits;
     private final List<PhoneStateListener> mPhoneStateListeners = new ArrayList<>();
+    
+    private final IThemeCallback mThemeCallback = new IThemeCallback.Stub() {
 
+        @Override
+        public void onThemeChanged(int themeMode, int color) {
+            onCallbackAdded(themeMode, color);
+        }
+
+        @Override
+        public void onCallbackAdded(int themeMode, int color) {
+            sTheme = color;
+        }
+    };
     /**
      * @param context everything needs a context :(
      */
@@ -244,6 +258,10 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
         }
         // Set the initial status of airplane mode toggle
         mAirplaneState = getUpdatedAirplaneToggleState();
+        mThemeManager = (ThemeManager) mContext.getSystemService(Context.THEME_SERVICE);
+        if (mThemeManager != null) {
+            mThemeManager.addCallback(mThemeCallback);
+        }
     }
 
     /**
@@ -293,6 +311,20 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
             mDialog.getWindow().getDecorView().setSystemUiVisibility(View.STATUS_BAR_DISABLE_EXPAND);
         }
     }
+     public static Context getContext(Context context) {
+            int themeMode = Settings.Secure.getInt(context.getContentResolver(),
+                    Settings.Secure.THEME_PRIMARY_COLOR, 0);
+            int accentColor = Settings.Secure.getInt(context.getContentResolver(),
+                    Settings.Secure.THEME_ACCENT_COLOR, 0);
+
+            if (themeMode == 0 && accentColor == 0) {
+                // Keep original theme
+                return context;
+            } else {
+                // Use our color engine theme
+                return new ContextThemeWrapper(context, sTheme);
+            }
+        }
 
     /**
      * Create the global actions dialog.
@@ -412,12 +444,12 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
 
         mAdapter = new MyAdapter();
 
-        AlertParams params = new AlertParams(mContext);
+        AlertParams params = new AlertParams(getContext(mContext));
         params.mAdapter = mAdapter;
         params.mOnClickListener = this;
         params.mForceInverseBackground = true;
 
-        GlobalActionsDialog dialog = new GlobalActionsDialog(mContext, params);
+        GlobalActionsDialog dialog = new GlobalActionsDialog(getContext(mContext), params);
         dialog.setCanceledOnTouchOutside(false); // Handled by the custom class.
 
         dialog.getListView().setItemsCanFocus(true);
@@ -969,7 +1001,7 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
 
         public View getView(int position, View convertView, ViewGroup parent) {
             Action action = getItem(position);
-            return action.create(mContext, convertView, parent, LayoutInflater.from(mContext));
+            return action.create(getContext(mContext), convertView, parent, LayoutInflater.from(mContext));
         }
     }
 
